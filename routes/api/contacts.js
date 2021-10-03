afe0c21f3ea7d38bbacc35f6b7fe0f4564e04189
Dmitry Karas <dmitrythekaras@gmail.com>
2021-10-03 15:15:13 +0300

    fix: add auth middleware to contacts router

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -2,31 +2,47 @@ const express = require("express");
 
 const router = express.Router();
 
-const { controllerWrapper, validation } = require("../../middlewares");
+const {
+  controllerWrapper,
+  validation,
+  authenticate,
+} = require("../../middlewares");
 const {
   joiContactSchema,
   joiUpdateFavoriteSchema,
 } = require("../../models/contact");
 const contactsController = require("../../controllers/contacts");
 
-router.get("/", controllerWrapper(contactsController.getContacts));
-router.get("/:contactId", controllerWrapper(contactsController.getContactById));
+router.get(
+  "/",
+  authenticate,
+  controllerWrapper(contactsController.getContacts)
+);
+router.get(
+  "/:contactId",
+  authenticate,
+  controllerWrapper(contactsController.getContactById)
+);
 router.post(
   "/",
+  authenticate,
   validation(joiContactSchema),
   controllerWrapper(contactsController.addContact)
 );
 router.delete(
   "/:contactId",
+  authenticate,
   controllerWrapper(contactsController.removeContact)
 );
 router.put(
   "/:contactId",
+  authenticate,
   validation(joiContactSchema),
   controllerWrapper(contactsController.updateContact)
 );
 router.patch(
   "/:contactId/favorite",
+  authenticate,
   validation(joiUpdateFavoriteSchema),
   controllerWrapper(contactsController.updateFavorite)
 );
