a5a636f18dba34d79de45a33e64ec0a1d8c14490
Dmitry Karas <dmitrythekaras@gmail.com>
2021-10-03 12:40:03 +0300

    feat(contacts): add favorite contacts filter

--- controllers/contacts/getContacts.js
+++ controllers/contacts/getContacts.js
@@ -2,7 +2,7 @@ const { sendSuccessRes } = require("../../utils");
 const { Contact } = require("../../models");
 
 const getContacts = async (req, res) => {
-  const contacts = await Contact.find();
+  const contacts = await Contact.find(req.query);
 
   sendSuccessRes(res, { contacts });
 };
