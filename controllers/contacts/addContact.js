8745bf625c2049c1db93a0be5fddae5131b3d18a
Dmitry Karas <dmitrythekaras@gmail.com>
2021-10-03 15:34:33 +0300

    fix: check for the uniqueness of the contact separately for each user

--- controllers/contacts/addContact.js
+++ controllers/contacts/addContact.js
@@ -1,8 +1,21 @@
 const { sendSuccessRes } = require("../../utils");
 const { Contact } = require("../../models");
+const { Conflict } = require("http-errors");
 
 const addContact = async (req, res) => {
-  const contact = await Contact.create(req.body);
+  const { _id } = req.user;
+  const { email, phone } = req.body;
+  const includedContacts = await Contact.find({
+    $or: [{ email }, { phone }],
+  });
+
+  includedContacts.forEach((contact) => {
+    if (contact.owner.toString() === _id.toString()) {
+      throw new Conflict("Contact already exist");
+    }
+  });
+
+  const contact = await Contact.create({ ...req.body, owner: _id });
 
   sendSuccessRes(res, { contact }, 201);
 };
