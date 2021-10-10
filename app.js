c8a7e538c9f83d6de978df7cf239cbc01b87090b
Dmitry Karas <dmitrythekaras@gmail.com>
2021-10-10 12:02:44 +0300

    feat(app): add serving static resources

--- app.js
+++ app.js
@@ -6,12 +6,12 @@ const contactsRouter = require("./routes/api/contacts");
 const authRouter = require("./routes/api/auth");
 
 const app = express();
-
 const formatsLogger = app.get("env") === "development" ? "dev" : "short";
 
 app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
+app.use(express.static("public"));
 app.use("/api/contacts", contactsRouter);
 app.use("/api/auth", authRouter);
 
