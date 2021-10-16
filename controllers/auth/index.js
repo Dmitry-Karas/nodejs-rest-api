69a0b7455cd5bf80655e0d7ba8fa3b7e3a92c135
Dmitry Karas <dmitrythekaras@gmail.com>
2021-10-16 16:44:15 +0300

    feat(controllers): add verification controller

--- controllers/auth/index.js
+++ controllers/auth/index.js
@@ -3,5 +3,6 @@ const login = require("./login");
 const logout = require("./logout");
 const current = require("./current");
 const avatars = require("./avatars");
+const verify = require("./verify");
 
-module.exports = { signup, login, logout, current, avatars };
+module.exports = { signup, login, logout, current, avatars, verify };
