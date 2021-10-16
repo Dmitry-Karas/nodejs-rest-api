6d32199debcfa659cbfe891d1aa51e91b9a79c8a
Dmitry Karas <dmitrythekaras@gmail.com>
2021-10-16 17:28:39 +0300

    feat(controllers): add verification email re-send controller

--- controllers/auth/index.js
+++ controllers/auth/index.js
@@ -4,5 +4,6 @@ const logout = require("./logout");
 const current = require("./current");
 const avatars = require("./avatars");
 const verify = require("./verify");
+const reSend = require("./reSend");
 
-module.exports = { signup, login, logout, current, avatars, verify };
+module.exports = { signup, login, logout, current, avatars, verify, reSend };
